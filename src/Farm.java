f5a4a388932b92a94a8814c1bdf9b16f7b0f6648
Tuhacek <tomca.tuhacek@gmail.com>
2025-01-30 10:58:46 +0100

    Methods of actions in the programs

--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,72 @@
+import Animals.Animal;
+import Animals.Size;
+import Flowers.Flower;
+
+
+import java.util.ArrayList;
+
 public class Farm {
 
 
+    private ArrayList<Animal> animals = new ArrayList<>();
+    private ArrayList<Flower> flowers = new ArrayList<>();
+    private int account;
+    private int fieldSize = 100;
 
+
+
+    public void plantFlower(Flower flower){
+        if(fieldSize - flower.getNeededArea()>0){
+            if (account> flo)
+        }
+    }
+
+
+    public void sellAnimals(Animal animal) {
+        if (animals.contains(animal)) {
+            if (animal.getSize() == Size.SMALL) {
+                animals.remove(animal);
+                account += 10000;
+            }
+        } else if (animals.contains(animal)) {
+            if (animal.getSize() == Size.BIG) {
+                animals.remove(animal);
+                account += 15000;
+            }
+        }
+    }
+
+
+    public void buyAnimal(Animal animal) {
+        int small = 0;
+        int big = 0;
+
+        for (Animal a : animals) {
+            if (a.getSize() == Size.SMALL) {
+                small++;
+            } else if (a.getSize() == Size.BIG) {
+                big++;
+            }
+        }
+        if (animal.getSize() == Size.SMALL) {
+            if (small > 10) {
+                if (account > 1000) {
+                    animals.add(animal);
+                    account -= 1000;
+                }
+            }
+        } else if (animal.getSize() == Size.BIG) {
+            if (big > 10) {
+                if (account > 10000) {
+                    animals.add(animal);
+                    account -= 10000;
+                }
+            }
+        }
+    }
+
+
+    public void watering (Flower flower){
+        
+    }
 }
